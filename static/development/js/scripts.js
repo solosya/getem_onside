5f8afcc9e57032fd96c2de55db611e4097e1a940
Lee Neenan <lneenan@pagemasters.com.au>
2017-09-16 13:46:17 +1000

    gallery layout complete.  url hashing works

--- static/development/js/scripts.js
+++ static/development/js/scripts.js
@@ -191,8 +191,6 @@ $('document').ready(function() {
 
     $("#owl-carousel").owlCarousel({
         items: 1,
-        // thumbs: true,
-        // thumbsPrerendered: true,
         URLhashListener:true,
         startPosition: 'URLHash',
         pagination: true,
