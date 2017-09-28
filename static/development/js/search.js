932ec39acdee4b531760d18b202f46ca17e55c07
Lee Neenan <lneenan@pagemasters.com.au>
2017-09-28 13:19:53 +1000

    FIXED?

--- static/development/js/search.js
+++ static/development/js/search.js
@@ -10,56 +10,56 @@ SearchController.Listing = (function ($) {
 
     var attachEvents = function () {
         
-        // $('.loadMoreArticles').on('click', function(e){
-        //     e.preventDefault();
-        //     var btnObj = $(this);
-        //     $.fn.Ajax_LoadSearchArticles({
-        //         'search': $('input.header__search-text').val(),
-        //         onSuccess: function(data, textStatus, jqXHR){
-        //               if (data.success == 1) {
-        //                 for (var i in data.articles) {
-        //                     data.articles[i]['containerClass'] = 'col-quarter';
+        $('.loadMoreArticles').on('click', function(e){
+            e.preventDefault();
+            var btnObj = $(this);
+            $.fn.Ajax_LoadSearchArticles({
+                'search': $('input.header__search-text').val(),
+                onSuccess: function(data, textStatus, jqXHR){
+                      if (data.success == 1) {
+                        for (var i in data.articles) {
+                            data.articles[i]['containerClass'] = 'col-quarter';
                             
-        //                     data.articles[i]['promotedClass'] = (data.articles[i].isPromoted == 1)? 'ad_icon' : '';
-        //                     data.articles[i]['hasArticleMediaClass'] = (data.articles[i].hasMedia == 1)? 'withImage__content' : 'without__image';
+                            data.articles[i]['promotedClass'] = (data.articles[i].isPromoted == 1)? 'ad_icon' : '';
+                            data.articles[i]['hasArticleMediaClass'] = (data.articles[i].hasMedia == 1)? 'withImage__content' : 'without__image';
                             
-        //                     data.articles[i]['blogClass']= '';
-        //                     if(data.articles[i].blog['id'] !== null) {
-        //                        data.articles[i]['blogClass']= 'card--blog_'+data.articles[i].blog['id'];
-        //                     } 
+                            data.articles[i]['blogClass']= '';
+                            if(data.articles[i].blog['id'] !== null) {
+                               data.articles[i]['blogClass']= 'card--blog_'+data.articles[i].blog['id'];
+                            } 
                             
-        //                     var ImageUrl = $.image({media:data.articles[i]['featuredMedia'], mediaOptions:{width: 500 ,height:350, crop: 'limit'} });
-        //                     data.articles[i]['imageUrl'] = ImageUrl;
+                            var ImageUrl = $.image({media:data.articles[i]['featuredMedia'], mediaOptions:{width: 500 ,height:350, crop: 'limit'} });
+                            data.articles[i]['imageUrl'] = ImageUrl;
                            
-        //                     var articleTemplate = Handlebars.compile(systemCardTemplate);
+                            var articleTemplate = Handlebars.compile(systemCardTemplate);
 
-        //                     var article = articleTemplate(data.articles[i]);
-        //                     $('.ajaxArticles').append(article);
+                            var article = articleTemplate(data.articles[i]);
+                            $('.ajaxArticles').append(article);
 
-        //                 }
-        //                 if(data.articles.length < 20) {
-        //                     $(btnObj).css('display', 'none');
-        //                 }
+                        }
+                        if(data.articles.length < 20) {
+                            $(btnObj).css('display', 'none');
+                        }
 
-        //                 bindSocialShareArticle();
-        //                 $(".card p, .card h1").dotdotdot();
+                        bindSocialShareArticle();
+                        $(".card p, .card h1").dotdotdot();
                         
-        //                  //Lazyload implement
-        //                 $("div.lazyload").lazyload({
-        //                     effect: "fadeIn"
-        //                 });
-        //             }
+                         //Lazyload implement
+                        $("div.lazyload").lazyload({
+                            effect: "fadeIn"
+                        });
+                    }
                 
                    
-        //         },
-        //         beforeSend: function(jqXHR, settings){
-        //             $(btnObj).html("Please wait...");
-        //         },
-        //         onComplete: function(jqXHR, textStatus){
-        //             $(btnObj).html("Load More");
-        //         }
-        //     });
-        // });
+                },
+                beforeSend: function(jqXHR, settings){
+                    $(btnObj).html("Please wait...");
+                },
+                onComplete: function(jqXHR, textStatus){
+                    $(btnObj).html("Load More");
+                }
+            });
+        });
         
         var bindSocialShareArticle = function () {
             $('.shareIcons').SocialShare({
